0ff38386b045105d5d9d5c6fd1c6c6bd47526737
Lukas Weber <l.alex.w@web.de>
2023-08-14 08:13:01 +0200

    add all other functionality

--- tests/test_images.py
+++ tests/test_images.py
@@ -5,34 +5,41 @@ from context import Images, NamedImages, NumpyImages, load_test_img, test_input
 
 
 class TestStitcher(unittest.TestCase):
+    # TODO: DYI, test np arrays, Factory
+    
     def test_numpy_image_input(self):
         images = NumpyImages([load_test_img("s1.jpg"), load_test_img("s2.jpg")])
-        for img in images:
-            print(img.shape)
-        images.sizes
-        images.names
+        
+        full_np_arrays = list(images)
+
+        np.testing.assert_array_equal(images.sizes, [(1246, 700), (1385, 700)])
+
+        low_sizes = images.get_scaled_img_sizes(Images.Resolution.LOW)
+        np.testing.assert_array_equal(low_sizes, [(422, 237), (469, 237)])
+        
+        low_np_arrays = list(images.resize(Images.Resolution.LOW))
+
+        ratio = images.get_ratio(Images.Resolution.MEDIUM, Images.Resolution.LOW)
+        self.assertEqual(ratio, 0.408248290463863)
 
     def test_named_image_input(self):
         images = NamedImages([test_input("s1.jpg"), test_input("s2.jpg")])
-        for img in images:
-            print(img.shape)
-        images.sizes
-        images.names
 
-    # def test_images(self):
-    #     images = Images([test_input("s1.jpg"), test_input("s2.jpg")])
-    #     _ = list(images.resize(images.read(), Images.Resolution.MEDIUM))
+        full_np_arrays = list(images)
 
-    #     np.testing.assert_array_equal(images.sizes, [(1246, 700), (1385, 700)])
+        np.testing.assert_array_equal(images.sizes, [(1246, 700), (1385, 700)])
 
-    #     low_sizes = images.get_scaled_img_sizes(Images.Resolution.LOW)
-    #     np.testing.assert_array_equal(low_sizes, [(422, 237), (469, 237)])
+        low_sizes = images.get_scaled_img_sizes(Images.Resolution.LOW)
+        np.testing.assert_array_equal(low_sizes, [(422, 237), (469, 237)])
+        
+        low_np_arrays = list(images.resize(Images.Resolution.LOW))
 
-    #     ratio = images.get_ratio(Images.Resolution.MEDIUM, Images.Resolution.LOW)
-    #     self.assertEqual(ratio, 0.408248290463863)
+        ratio = images.get_ratio(Images.Resolution.MEDIUM, Images.Resolution.LOW)
+        self.assertEqual(ratio, 0.408248290463863)
 
-    #     self.assertEqual(Images.Resolution.LOW.name, "LOW")
-    #     self.assertEqual(Images.Resolution.LOW.value, 0.1)
+    def test_images(self):
+        self.assertEqual(Images.Resolution.LOW.name, "LOW")
+        self.assertEqual(Images.Resolution.LOW.value, 0.1)
 
 
 def start_test():
